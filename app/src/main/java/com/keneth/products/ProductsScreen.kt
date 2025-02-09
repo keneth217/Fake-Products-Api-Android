6563ed830b891aae1d38fe5ffeb6f8d8847a3971
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-09 21:56:15 +0300

    use fake api store to display products,create products data class,viewmodel,retrofit api call

---
+++ app/src/main/java/com/keneth/products/ProductsScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.products
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductsScreen(){
+//    val viewModel: ProductsViewModel = viewModel()
+//    val products = viewModel.products.observeAsState()
+
+}
