062f3f1895b9aea707f450f2ba44ea9c8352fbdb
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-09 21:56:39 +0300

    use fake api store to display products,create products data class,viewmodel,retrofit api call

---
+++ app/src/main/java/com/keneth/products/ProductsViewModel.kt
@@ -0,0 +1,50 @@
+package com.keneth.products
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class ProductsViewModel : ViewModel() {
+    private val _productsState = mutableStateOf(ProductsState())
+    val productsState: State<ProductsState> get() = _productsState
+
+    init {
+        fetchProducts()
+    }
+
+    fun fetchProducts() {
+        _productsState.value = _productsState.value.copy(isLoading = true)
+
+        viewModelScope.launch {
+            try {
+                val response = RetrofitInstance.api.getProducts()
+
+                if (response.isSuccessful && response.body() != null) {
+                    _productsState.value = _productsState.value.copy(
+                        productsList = response.body()!!,
+                        isLoading = false,
+                        error = null
+                    )
+                } else {
+                    _productsState.value = _productsState.value.copy(
+                        isLoading = false,
+                        error = "Failed to fetch products. Error: ${response.message()}"
+                    )
+                }
+            } catch (e: Exception) {
+                _productsState.value = _productsState.value.copy(
+                    isLoading = false,
+                    error = "An error occurred: ${e.localizedMessage}"
+                )
+            }
+        }
+    }
+
+    data class ProductsState(
+        val productsList: List<ProductsItem> = emptyList(),
+        val isLoading: Boolean = false,
+        val error: String? = null
+    )
+}
