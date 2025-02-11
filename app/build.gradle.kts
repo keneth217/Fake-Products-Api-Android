215ecce6ef77a25319c943f05b95a3c7b49b23ce
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-11 16:55:48 +0300

    navigation controllers,drawers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -10,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.keneth.products"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -41,6 +42,9 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.protolite.well.known.types)
+    implementation(libs.androidx.compose.material3)
     val nav_version = "2.8.6"
     val lifecycle_version = "2.8.7"
     val retrofit_version = "2.9.0"
