44356119b82a9a768fc15efe85e371637c8d4726
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-11 16:46:40 +0300

    navigation controllers,drawers

---
+++ app/src/main/java/com/keneth/products/screen/Screens.kt
@@ -0,0 +1,42 @@
+package com.keneth.products.screen
+
+import androidx.annotation.DrawableRes
+import com.keneth.products.R
+
+sealed class Screens(val route: String, val title: String? = null, open val icon: Int? = null) {
+    object SplashScreen : Screens("splash_screen")
+
+    object ProductsScreen : Screens("products_screen")
+
+    object ProductDetailsScreen : Screens("product_details_screen")
+
+
+    sealed class DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes override val icon: Int) :
+        Screens(dTitle, dRoute) {
+        object Account : DrawerScreen(
+            "Account",
+            "account",
+            R.drawable.account
+        )
+
+        object Subscription : DrawerScreen(
+            "Subscription",
+            "subscribe",
+            R.drawable.subscribe
+        )
+
+        object AddAccount : DrawerScreen(
+            "Add Account",
+            "add_account",
+            R.drawable.add_account
+        )
+    }
+
+
+    val screensInDrawer = listOf(
+        Screens.DrawerScreen.Account,
+        Screens.DrawerScreen.Subscription,
+        Screens.DrawerScreen.AddAccount
+    )
+
+}
