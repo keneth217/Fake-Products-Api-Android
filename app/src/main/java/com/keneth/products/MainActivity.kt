21ce21010a0dd2a0885e03d9e231d3a2bae6a612
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-11 16:45:14 +0300

    navigation controllers,drawers

--- app/src/main/java/com/keneth/products/MainActivity.kt
+++ app/src/main/java/com/keneth/products/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.keneth.products
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.keneth.products.screen.ProductMainApp
 import com.keneth.products.ui.theme.FakeProductsTheme
 
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+//        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FakeProductsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold() {
+                    ProductMainApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FakeProductsTheme {
-        Greeting("Android")
     }
 }
