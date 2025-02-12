8da87cdc4c0583a03baf5bc14f64dcb79ab30be3
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-12 17:25:32 +0300

    navigation controllers,drawers,separate  composables

--- app/src/main/java/com/keneth/products/screen/SplashScreen.kt
+++ app/src/main/java/com/keneth/products/screen/SplashScreen.kt
@@ -1,51 +1,68 @@
 package com.keneth.products.screen
 
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.keneth.products.R
-
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
     val DarkPink = Color(0xFFe496ee)
+
     LaunchedEffect(Unit) {
-        delay(3000) // Wait for 3 seconds
+        delay(3000)
         navController.navigate(Screens.ProductsScreen.route) {
             popUpTo(Screens.SplashScreen.route) { inclusive = true }
         }
-
     }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(DarkPink),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(250.dp)
         ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background), // Replace with your logo
-            contentDescription = "Splash Logo",
-            modifier = Modifier.size(150.dp)
-        )
-        Text(
-            "WELCOME TO ${stringResource(id = R.string.app_name)}",
-            style = MaterialTheme.typography.headlineLarge,
-            color = Color.White
-        )
+            Image(
+                painter = painterResource(id = R.drawable.women),
+                contentDescription = "App Logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .shadow(16.dp, shape = CircleShape)
+            )
 
+            Text(
+                text = "WELCOME TO ${stringResource(id = R.string.app_name)}",
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
     }
-
 }
