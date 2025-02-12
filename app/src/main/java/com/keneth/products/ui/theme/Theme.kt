a24b3c50b3010919253123d1310a06f27387353b
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-12 17:08:28 +0300

    navigation controllers,drawers,separate  composables

--- app/src/main/java/com/keneth/products/ui/theme/Theme.kt
+++ app/src/main/java/com/keneth/products/ui/theme/Theme.kt
@@ -12,25 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Red,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink100
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Green
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
