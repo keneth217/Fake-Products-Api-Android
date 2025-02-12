db8169eed59d823cf2e996b6fff461498cca689d
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-12 17:24:20 +0300

    navigation controllers,drawers,separate  composables

--- app/src/main/java/com/keneth/products/screen/ProductsScreen.kt
+++ app/src/main/java/com/keneth/products/screen/ProductsScreen.kt
@@ -1,33 +1,22 @@
 package com.keneth.products.screen
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import coil3.compose.rememberAsyncImagePainter
 import com.keneth.products.model.ProductsViewModel
 import com.keneth.products.data.ProductsItem
-import kotlinx.coroutines.launch
 
 
-
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProductsScreen(
     modifier: Modifier = Modifier,
@@ -36,62 +25,17 @@ fun ProductsScreen(
 ) {
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-
+    val navController = rememberNavController()
+    val currentRoute = navController.currentBackStackEntry?.destination?.route ?: "home"
     ModalNavigationDrawer(
         drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                Column(
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    Spacer(Modifier.height(12.dp))
-                    // Add your drawer content here
-                    Text("Categories", modifier = Modifier.padding(16.dp))
-                    Divider()
-                    NavigationDrawerItem(
-                        label = { Text("Electronics") },
-                        selected = false,
-                        onClick = { /* Handle category selection */ }
-                    )
-                    NavigationDrawerItem(
-                        label = { Text("Jewelry") },
-                        selected = false,
-                        onClick = { /* Handle category selection */ }
-                    )
-                }
-            }
-        }
+        drawerContent = { ModalDrawerSheet { AppDrawer(drawerState, scope) } }
     ) {
         Scaffold(
             topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Text(
-                            "Products",
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = {
-                            scope.launch {
-                                if (drawerState.isClosed) drawerState.open() else drawerState.close()
-                            }
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.Menu,
-                                contentDescription = "Menu"
-                            )
-                        }
-                    },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
+                TopBar(scope, drawerState)
+            },
+            bottomBar = { BottomNavBar(navController, currentRoute) }
         ) { innerPadding ->
             Box(
                 modifier = modifier
@@ -100,26 +44,35 @@ fun ProductsScreen(
                     .padding(16.dp)
             ) {
                 when {
-                    viewState.isLoading -> {
-                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                    }
-                    viewState.error != null -> {
-                        Text(
-                            text = viewState.error,
-                            modifier = Modifier.align(Alignment.Center)
+                    viewState.isLoading -> CircularProgressIndicator(
+                        modifier = Modifier.align(
+                            Alignment.Center
                         )
-                    }
-                    else -> {
-                        ProductList(
-                            products = viewState.productsList,
-                            navigateToProductsDetails = navigateToProductsDetails
-                        )
-                    }
+                    )
+
+                    viewState.error != null -> Text(
+                        text = viewState.error,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+
+                    else -> ProductList(
+                        products = viewState.productsList,
+                        navigateToProductsDetails = navigateToProductsDetails
+                    )
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -157,9 +110,7 @@ fun ProductItem(
             modifier = Modifier.fillMaxWidth()
         ) {
             Column(
-                modifier = Modifier
-                    .padding(8.dp),
-
+                modifier = Modifier.padding(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
@@ -169,18 +120,13 @@ fun ProductItem(
                         .size(128.dp)
                         .padding(8.dp)
                 )
-
                 Text(
                     text = product.title,
                     style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier.padding(4.dp)
                 )
-
                 Row(modifier = Modifier.padding(top = 8.dp)) {
-                    Text(
-                        text = "$${product.price}",
-                        modifier = Modifier.padding(horizontal = 4.dp)
-                    )
+                    Text(text = "$${product.price}", modifier = Modifier.padding(horizontal = 4.dp))
                     Text(
                         text = "⭐ ${product.rating.rate} (${product.rating.count})",
                         modifier = Modifier.padding(horizontal = 4.dp)
