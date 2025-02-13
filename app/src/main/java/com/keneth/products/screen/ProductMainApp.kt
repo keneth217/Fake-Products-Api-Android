baa608699ea9e193c34daad5a144f91e99017157
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-13 17:25:06 +0300

    dummy json api

--- app/src/main/java/com/keneth/products/screen/ProductMainApp.kt
+++ app/src/main/java/com/keneth/products/screen/ProductMainApp.kt
@@ -6,8 +6,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.keneth.products.data.ProductsItem
-import com.keneth.products.data.Rating
+import com.keneth.products.data.Clothes.ProductsItem
+import com.keneth.products.data.Clothes.Rating
+import com.keneth.products.data.Juice.Dimensions
+import com.keneth.products.data.Juice.JuiceProducts
+import com.keneth.products.data.Juice.Meta
+import com.keneth.products.model.JuiceViewModel
 import com.keneth.products.model.ProductsViewModel
 
 @Composable
@@ -15,6 +19,9 @@ fun ProductMainApp() {
     val navController = rememberNavController()
     val productsViewModel: ProductsViewModel = viewModel()
     val viewState by productsViewModel.productsState
+
+    val juiceViewModel: JuiceViewModel = viewModel()
+    val fruitViewState by juiceViewModel.juiceState
 
     NavHost(
         navController = navController,
@@ -33,10 +40,54 @@ fun ProductMainApp() {
                 modifier = Modifier
             )
         }
+
+
         composable(route = Screens.ProductDetailsScreen.route) {
             val product = navController.previousBackStackEntry?.savedStateHandle?.get<ProductsItem>("products")
                 ?: ProductsItem("", "", 0, "", 0.0, Rating(0, 0.0), "")
             ProductDetailsScreen(product,navController)
         }
+
+        composable(route = Screens.FruitsScreen.route) {
+            FruitsScreen(
+                viewState = fruitViewState,
+                navigateToJuiceDetails = {
+                    navController.currentBackStackEntry?.savedStateHandle?.set("juice", it)
+                    navController.navigate(Screens.JuiceDetailsScreen.route)
+                },
+                modifier = Modifier
+            )
+        }
+
+        composable(route = Screens.JuiceDetailsScreen.route) {
+            val juice = navController.previousBackStackEntry?.savedStateHandle?.get<JuiceProducts>("juice")
+                ?: JuiceProducts(
+                    availabilityStatus = "",
+                    brand = "",
+                    category = "",
+                    description = "",
+                    dimensions = Dimensions(0.0, 0.0, 0.0), // Assuming this structure
+                    discountPercentage = 0.0,
+                    id = 0,
+                    images = emptyList(),
+                    meta = Meta("", "","",""), // Assuming this structure
+                    minimumOrderQuantity = 0,
+                    price = 0.0,
+                    rating = 0.0,
+                    returnPolicy = "",
+                    reviews = emptyList(),
+                    shippingInformation = "",
+                    sku = "",
+                    stock = 0,
+                    tags = emptyList(),
+                    thumbnail = "",
+                    title = "",
+                    warrantyInformation = "",
+                    weight = 0
+                )
+
+            JuiceDetailsScreen(juice, navController)
+        }
+
     }
 }
