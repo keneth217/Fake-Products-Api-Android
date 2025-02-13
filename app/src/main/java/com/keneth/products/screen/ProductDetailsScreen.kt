bdc181f7d2b9744eecee10dd8d788703bcae6b98
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-13 17:24:52 +0300

    dummy json api

--- app/src/main/java/com/keneth/products/screen/ProductDetailsScreen.kt
+++ app/src/main/java/com/keneth/products/screen/ProductDetailsScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -21,7 +20,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,10 +27,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import coil3.compose.rememberAsyncImagePainter
-import com.keneth.products.data.Products
-import com.keneth.products.data.ProductsItem
+import com.keneth.products.data.Clothes.ProductsItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -107,33 +103,7 @@ fun ProductDetailsScreen(
             }
 
             Text(
-                text = product.description + "Hi Devs, in this article i am going to show you how you can design bottom navigation bar in jetpack compose using navigation component(nav controller and nav host).\n" +
-                        "\n" +
-                        "For this article I have designed bottom navigation bar just like LinkedIn. In jetpack compose you don’t need to define your navigation under navigation directory and create its navigation graph. Navigation in jetpack compose is different from xml navigation where we need to define the fragments and graph. Add below dependency in your app level gradle file and rebuild your app.\n" +
-                        "\n" +
-                        "implementation \"androidx.navigation:navigation-compose:2.4.0-beta02\"\n" +
-                        "Hi Devs, in this article i am going to show you how you can design bottom navigation bar in jetpack compose using navigation component(nav controller and nav host).\n" +
-                        "\n" +
-                        "For this article I have designed bottom navigation bar just like LinkedIn. In jetpack compose you don’t need to define your navigation under navigation directory and create its navigation graph. Navigation in jetpack compose is different from xml navigation where we need to define the fragments and graph. Add below dependency in your app level gradle file and rebuild your app.\n" +
-                        "\n" +
-                        "implementation \"androidx.navigation:navigation-compose:2.4.0-beta02\"\n" +
-                        "Hi Devs, in this article i am going to show you how you can design bottom navigation bar in jetpack compose using navigation component(nav controller and nav host).\n" +
-                        "\n" +
-                        "For this article I have designed bottom navigation bar just like LinkedIn. In jetpack compose you don’t need to define your navigation under navigation directory and create its navigation graph. Navigation in jetpack compose is different from xml navigation where we need to define the fragments and graph. Add below dependency in your app level gradle file and rebuild your app.\n" +
-                        "\n" +
-                        "implementation \"androidx.navigation:navigation-compose:2.4.0-beta02\"\n" +
-                        "Hi Devs, in this article i am going to show you how you can design bottom navigation bar in jetpack compose using navigation component(nav controller and nav host).\n" +
-                        "\n" +
-                        "For this article I have designed bottom navigation bar just like LinkedIn. In jetpack compose you don’t need to define your navigation under navigation directory and create its navigation graph. Navigation in jetpack compose is different from xml navigation where we need to define the fragments and graph. Add below dependency in your app level gradle file and rebuild your app.\n" +
-                        "\n" +
-                        "implementation \"androidx.navigation:navigation-compose:2.4.0-beta02\"\n" +
-                        "Hi Devs, in this article i am going to show you how you can design bottom navigation bar in jetpack compose using navigation component(nav controller and nav host).\n" +
-                        "\n" +
-                        "For this article I have designed bottom navigation bar just like LinkedIn. In jetpack compose you don’t need to define your navigation under navigation directory and create its navigation graph. Navigation in jetpack compose is different from xml navigation where we need to define the fragments and graph. Add below dependency in your app level gradle file and rebuild your app.\n" +
-                        "\n" +
-                        "implementation \"androidx.navigation:navigation-compose:2.4.0-beta02\"\n" +
-                        "After rebuilding your app create a separate directory for bottom navigation related files. Now create sealed class with name BottomNavItem with bottom navigation item title, item icon and item route which we will use later for navigation between screens just like below:",
-                textAlign = TextAlign.Justify,
+                text = product.description ,   textAlign = TextAlign.Justify,
                 style = MaterialTheme.typography.bodyLarge,
                 modifier = Modifier.verticalScroll(rememberScrollState()).padding(top=20.dp)
             )
