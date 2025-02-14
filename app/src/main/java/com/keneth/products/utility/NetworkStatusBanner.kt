92ef7b0fcf90bbea28ec18869b0eb1d04443c8da
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-14 22:51:16 +0300

    fetch dummy apis,add network check,connected the routes in bottom bar and drawer

---
+++ app/src/main/java/com/keneth/products/utility/NetworkStatusBanner.kt
@@ -0,0 +1,32 @@
+package com.keneth.products.utility
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.keneth.products.model.NetworkViewModel
+
+@Composable
+fun NetworkStatusBanner(viewModel: NetworkViewModel) {
+    val isConnected by viewModel.isConnected.collectAsState()
+
+    if (!isConnected) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.7f))
+                .clickable { },
+            contentAlignment = Alignment.Center
+        ) {
+            NoInternetContent(
+                onRetry = { viewModel.checkInitialConnection() }
+            )
+        }
+    }
+}
