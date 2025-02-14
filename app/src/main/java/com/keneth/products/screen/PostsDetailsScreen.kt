5552db1cc8c109b3b2ab7c488342fa59f3a9989d
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-14 22:52:57 +0300

    fetch dummy apis,add network check,connected the routes in bottom bar and drawer

---
+++ app/src/main/java/com/keneth/products/screen/PostsDetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.products.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.keneth.products.data.Posts.Post
+
+@Composable
+fun PostsDetailsScreen(posts: Post, navController: NavHostController) {
+
+}
