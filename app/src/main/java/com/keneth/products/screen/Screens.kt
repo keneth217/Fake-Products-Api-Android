400eb29eb6267f3d90a0939a9318ccdb6ab450df
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-14 22:53:50 +0300

    fetch dummy apis,add network check,connected the routes in bottom bar and drawer

--- app/src/main/java/com/keneth/products/screen/Screens.kt
+++ app/src/main/java/com/keneth/products/screen/Screens.kt
@@ -1,44 +1,23 @@
 package com.keneth.products.screen
 
-import androidx.annotation.DrawableRes
-import com.keneth.products.R
 
 sealed class Screens(val route: String, val title: String? = null, open val icon: Int? = null) {
-    object SplashScreen : Screens("splash_screen")
+    object SplashScreen : Screens("splash_screen",)
 
-    object ProductsScreen : Screens("products_screen")
+    object ProductsScreen : Screens("products_screen","Products")
 
-    object ProductDetailsScreen : Screens("product_details_screen")
-    object FruitsScreen : Screens("fruits_screen")
-    object JuiceDetailsScreen : Screens("juice_details_screen")
+    object ProductDetailsScreen : Screens("product_details_screen","Product Details")
+    object FruitsScreen : Screens("fruits_screen","Fruits")
+    object JuiceDetailsScreen : Screens("juice_details_screen","Juice Details")
 
-
-    sealed class DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes override val icon: Int) :
-        Screens(dTitle, dRoute) {
-        object Account : DrawerScreen(
-            "Account",
-            "account",
-            R.drawable.account
-        )
-
-        object Subscription : DrawerScreen(
-            "Subscription",
-            "subscribe",
-            R.drawable.subscribe
-        )
-
-        object AddAccount : DrawerScreen(
-            "Add Account",
-            "add_account",
-            R.drawable.add_account
-        )
-    }
+    object QuotesScreen : Screens("quotes_screen","Quotes")
+    object PostsScreen : Screens("posts_screen","Posts")
+    object CommentsScreen : Screens("comments_screen","Comments")
+    object QuotesDetailsScreen : Screens("quotes_details_screen","Quotes Details")
+    object PostsDetailsScreen : Screens("posts_details_screen","Posts Details")
+    object CommentsDetailsScreen : Screens("comments_details_screen","Comments Details")
 
 
-    val screensInDrawer = listOf(
-        Screens.DrawerScreen.Account,
-        Screens.DrawerScreen.Subscription,
-        Screens.DrawerScreen.AddAccount
-    )
+
 
 }
